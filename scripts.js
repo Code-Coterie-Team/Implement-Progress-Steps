2f999fcaf5733597617a23ded9e54647000c7b0c
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-09-25 20:34:36 +0330

    add more than one time usage of progress line

--- scripts.js
+++ scripts.js
@@ -6,6 +6,12 @@ const next = document.querySelector('.next')
 
 let activeNow = 1;
 
+if (activeNow == 1){
+    prev.disabled = true;
+} else if (activeNow == 4){
+    next.disabled = true;
+}
+
 function update(status){
     if (status){
         activeNow++;
@@ -15,15 +21,18 @@ function update(status){
                 case 2:
                     var curCircle = document.querySelector('.two');
                     curCircle.classList.add('activeBtn');
+                    prev.disabled = false;
                     break;
                 case 3:
                     var curCircle = document.querySelector('.three');
                     curCircle.classList.add('activeBtn');
+                    prev.disabled = false;
                     break;
                 case 4:
                     var curCircle = document.querySelector('.four');
                     curCircle.classList.add('activeBtn');
                     next.disabled = true;
+                    prev.disabled = false;
                     break;
             }
         }
@@ -39,14 +48,17 @@ function update(status){
                 case 3:
                     var curCircle = document.querySelector('.four');
                     curCircle.classList.remove('activeBtn');
+                    next.disabled = false;
                     break;
                 case 1:
                     var curCircle = document.querySelector('.two');
                     curCircle.classList.remove('activeBtn');
                     prev.disabled = true;
+                    next.disabled = false;
                     break;
             }
-        } 
+        }
     }
+    
 }
 
