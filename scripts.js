3b587ec40169ce2ea0bf4e5d7b0fe239b06ffae7
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-09-27 16:47:17 +0330

    add some changes

--- scripts.js
+++ scripts.js
@@ -21,47 +21,46 @@ function update(status){
     if (status){
         activeNow++;
         console.log(activeNow)
-        if (activeNow <= 4){
-            switch (activeNow){
-                case 2:
-                    var curCircle = document.querySelector('.two');
-                    curCircle.classList.add('activeBtn');
-                    prev.disabled = false;
-                    break;
-                case 3:
-                    var curCircle = document.querySelector('.three');
-                    curCircle.classList.add('activeBtn');
-                    prev.disabled = false;
-                    break;
-                case 4:
-                    var curCircle = document.querySelector('.four');
-                    curCircle.classList.add('activeBtn');
-                    next.disabled = true;
-                    prev.disabled = false;
-                    break;
-            }
+        if (activeNow > 4){
+            return
+        }
+        prev.disabled = false;
+        switch (activeNow){
+            case 2:
+                var curCircle = document.querySelector('.two');
+                curCircle.classList.add('activeBtn');
+                break;
+            case 3:
+                var curCircle = document.querySelector('.three');
+                curCircle.classList.add('activeBtn');
+                break;
+            case 4:
+                var curCircle = document.querySelector('.four');
+                curCircle.classList.add('activeBtn');
+                next.disabled = true;
+                break;
         }
     } else {
         activeNow--;
         console.log(activeNow)
-        if (activeNow > 0){
-            switch (activeNow){
-                case 2:
-                    var curCircle = document.querySelector('.three');
-                    curCircle.classList.remove('activeBtn');
-                    break;
-                case 3:
-                    var curCircle = document.querySelector('.four');
-                    curCircle.classList.remove('activeBtn');
-                    next.disabled = false;
-                    break;
-                case 1:
-                    var curCircle = document.querySelector('.two');
-                    curCircle.classList.remove('activeBtn');
-                    prev.disabled = true;
-                    next.disabled = false;
-                    break;
-            }
+        if (activeNow <= 0) return;
+        
+        switch (activeNow){
+            case 2:
+                var curCircle = document.querySelector('.three');
+                curCircle.classList.remove('activeBtn');
+                break;
+            case 3:
+                var curCircle = document.querySelector('.four');
+                curCircle.classList.remove('activeBtn');
+                next.disabled = false;
+                break;
+            case 1:
+                var curCircle = document.querySelector('.two');
+                curCircle.classList.remove('activeBtn');
+                prev.disabled = true;
+                next.disabled = false;
+                break;
         }
     }
     updateActiveLine()
